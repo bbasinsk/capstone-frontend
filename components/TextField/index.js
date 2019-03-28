57cbd98672790e9e6c2b43f9a9e1d374283f6df0
Ben Basinski <benbasinski97@gmail.com>
2019-03-27 22:33:59 -0700

    add realtime text-box

--- components/TextField/index.js
+++ components/TextField/index.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import { Subscription } from 'react-apollo';
-import { dump } from '../../libraries/helpers';
+import { Subscription, Mutation } from 'react-apollo';
+import { useState } from 'react';
 
 const GET_TEXT = gql`
   subscription {
@@ -10,15 +11,51 @@ const GET_TEXT = gql`
   }
 `;
 
-const TextField = () => (
-  <Subscription subscription={GET_TEXT}>
-    {({ loading, error, data }) => {
-      if (loading) return 'Loading...';
-      if (error) return `Error! ${error.message}`;
+const UPDATE_TEXT = gql`
+  mutation updateMeeting($id: Int, $text: String) {
+    update_meeting(where: { id: { _eq: $id } }, _set: { text_field: $text }) {
+      affected_rows
+      returning {
+        id
+        text_field
+      }
+    }
+  }
+`;
 
-      return <pre>{dump(data)}</pre>;
-    }}
-  </Subscription>
+const TextField = ({ initialValue }) => {
+  const [state, setState] = useState(initialValue);
+  return (
+    <div>
+      <h2>Realtime Textbox</h2>
+      <div>Value: {initialValue}</div>
+      <Mutation mutation={UPDATE_TEXT}>
+        {updateText => (
+          <textarea
+            onChange={e => {
+              const { value } = e.target;
+              setState(value);
+              updateText({ variables: { text: value, id: 1 } });
+            }}
+            value={state}
+          />
+        )}
+      </Mutation>
+    </div>
+  );
+};
+TextField.propTypes = {
+  initialValue: PropTypes.string.isRequired
+};
+
+export default () => (
+  <div>
+    <Subscription subscription={GET_TEXT}>
+      {({ loading, error, data }) => {
+        if (loading) return 'Loading...';
+        if (error) return `Error! ${error.message}`;
+        return <TextField initialValue={data.meeting[0].text_field} />;
+      }}
+    </Subscription>
+  </div>
 );
-
-export default TextField;
