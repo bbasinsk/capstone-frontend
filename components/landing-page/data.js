757c605f52436d0297056205e6b450155a34d0b5
Ben Basinski <benbasinski97@gmail.com>
2019-05-03 12:15:58 -0700

    update step copy

--- components/landing-page/data.js
+++ components/landing-page/data.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 export const steps = [
   {
-    title: '1. Create agenda',
+    title: '1. Create an agenda to structure your meeting',
     content:
       'Define what needs to be discussed in this meeting, how long it should take, and who the point person is.',
     src: '/static/images/create.svg'
   },
   {
-    title: '2. Collaborate in real-time',
+    title: '2. Collaborate in real-time with your team',
     content:
       'Collaborate with your team on the meeting document. Check off agenda items as they’re completed to keep a record of your team’s progress.',
     src: '/static/images/collaborate.svg'
   },
   {
-    title: '3. Send auto-generated summaries',
+    title: '3. Send auto-generated meeting summaries',
     content:
       'Once the meeting is over, you’ll receive a summary document that includes what was accomplished. Send this to your team!',
     src: '/static/images/summary.svg'
