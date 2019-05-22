6a4096f79a9063c6671e963fdc66a916b250c293
Ben Basinski <benbasinski97@gmail.com>
2019-05-22 08:17:23 -0700

    add admin secret to backend graphql link

--- server/events.js
+++ server/events.js
@@ -23,7 +23,10 @@ const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://meeting-magic-backend.herokuapp.com/v1alpha1/graphql',
     credentials: 'include',
-    fetch
+    fetch,
+    headers: {
+      'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET
+    }
   }),
   cache: new InMemoryCache()
 });
