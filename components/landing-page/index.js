1f84ea091e5604d96abf22185c2e410318f905ed
josephktsai <josephkt@uw.edu>
2019-05-22 15:48:31 -0700

    Update landing page footer with new footer component

--- components/landing-page/index.js
+++ components/landing-page/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import moment from 'moment';
-import { Layout } from 'antd';
 import Banner from './banner';
 import Steps from './steps';
 import Customers from './customers';
 import TryNow from './try-now';
+import Footer from '../footer';
 
 const LandingPage = () => (
   <div className="home-page">
@@ -12,18 +11,7 @@ const LandingPage = () => (
     <Steps />
     <Customers />
     <TryNow />
-    <Layout.Footer style={{ textAlign: 'center' }}>
-      <p>NeatMeet ©{moment().year()}</p>
-      <p>
-        <a href="/terms" alt="Link to terms and conditions">
-          Terms and Conditions
-        </a>
-        {' | '}
-        <a href="/privacy" alt="Link to terms and conditions">
-          Privacy Policy
-        </a>
-      </p>
-    </Layout.Footer>
+    <Footer />
   </div>
 );
 
