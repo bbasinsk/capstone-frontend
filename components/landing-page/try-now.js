def67242757743a70011ef4f78326fd5e986a598
josephktsai <josephkt@uw.edu>
2019-05-30 19:11:09 -0700

    Remove ids and replace with working selectors

--- components/landing-page/try-now.js
+++ components/landing-page/try-now.js
@@ -3,10 +3,7 @@ import Link from 'next/link';
 import { Button } from 'antd';
 
 const TryNow = () => (
-  <div
-    className="home-layout-wrapper home-try-wrapper"
-    id="trusted-customers-wrapper"
-  >
+  <div className="home-layout-wrapper home-try-wrapper">
     <Link href="/create">
       <Button type="primary" size="large">
         Start for free
